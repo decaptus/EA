1a85de95a698e704b746977d64b190d522163d00
decaptus <72303216+decaptus@users.noreply.github.com>
2021-09-30 19:58:27 +0200

    reset

--- config/vars.ts
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-
-export const default_bg               = "#2456a5"
-export const default_light_font_color = "white"
-export const default_lg_font_size     = 11
--- index.ts
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-import * as defaults from './config/vars'
-
-console.log(process.env.PORT);
-console.log(defaults.default_bg)
