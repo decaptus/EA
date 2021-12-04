c3ddf55235260208b7746ca93da3abef669aaa8e
Sergio Gras <sergiogras99@gmail.com>
2021-12-04 22:51:26 +0100

    file selector

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Axios from 'axios'
 import { useDispatch } from 'react-redux';
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
@@ -16,6 +17,7 @@ const initialState = { name: '', lastName: '', email: '', password: '', confirmP
 const Auth = () => {
   const [form, setForm] = useState(initialState);
   const [isSignup, setIsSignup] = useState(false);
+  const [imageSelected, setImageSelected] = useState();
   const dispatch = useDispatch();
   const history = useHistory();
   const classes = useStyles();
@@ -57,6 +59,13 @@ const Auth = () => {
 
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
+  const uploadImage = (files) => {
+    const formData = new FormData()
+    formData.append("file", files[0])
+    formData.append("upload_preset", " ")
+    
+  }
+
   return (
     <Container component="main" maxWidth="xs">
       <Paper className={classes.paper} elevation={3}>
@@ -74,7 +83,27 @@ const Auth = () => {
             )}
             <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
             <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
-            { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
+            { isSignup && (
+              <>
+            <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />
+            <Grid item >
+            <Typography className={classes.margin}> Select a picture  </Typography> 
+            </Grid>
+            
+            <input type="file" className={classes.margin} onChange={(event) => {
+              uploadImage(event.target.files)
+            }}></input >
+
+            </>
+            
+
+
+
+            
+
+        
+
+            )}
           </Grid>
           <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
             { isSignup ? 'Sign Up' : 'Sign In' }
@@ -99,7 +128,14 @@ const Auth = () => {
           </Grid>
         </form>
       </Paper>
+
+      
     </Container>
+    
+
+
+
+    
   );
 };
 
